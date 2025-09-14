a8160ec3e36ebf38bd378ed93326a78ca474f38b
Glover05 <fredoglover14@gmail.com>
2025-09-14 10:43:32 +0300

    first commit du projet Maven

---
+++ src/main/java/com/faniry/App.java
@@ -0,0 +1,13 @@
+package com.faniry;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
